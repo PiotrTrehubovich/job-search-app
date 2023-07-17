6a0ab698585b6513fd6bd3887116944915b8db0d
Piotr Trehubovich <petertregubovich@gmail.com>
2023-07-17 22:30:13 +0200

    fix: errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Providers from "@/providers";
-import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Providers>
-      <NextNProgress />
       <Component {...pageProps} />
     </Providers>
   );
